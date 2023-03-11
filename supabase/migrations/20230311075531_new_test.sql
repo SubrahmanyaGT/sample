fb88ad59a04c67b0f0b75302e0477c0db8f44c1f
Subrahmanya G T <Subrahmanya@aglinthq.com>
2023-03-11 13:34:43 +0530

    circle ci

--- supabase/migrations/20230311075531_new_test.sql
+++ supabase/migrations/20230311075531_new_test.sql
@@ -1,4 +1,4 @@
 create table public.tests (
-    id integer primary key generated always as identity,
-    name text
+    -- id integer primary key generated always as identity,
+    -- name text
 );
